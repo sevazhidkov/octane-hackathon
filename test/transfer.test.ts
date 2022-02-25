1f0a2ba0538936a5454a8467f9a731aa2b8957fd
Seva Zhidkov <seva@zhidkoff.com>
2022-02-25 22:53:05 +0300

    add test for arbitrary transfers and duplicate transactions

--- test/transfer.test.ts
+++ test/transfer.test.ts
@@ -1,16 +1,14 @@
-import {expect, use} from 'chai';
+import { expect } from 'chai';
 import { testApiHandler } from 'next-test-api-route-handler';
 import {
     Keypair, PublicKey, Connection,
     Transaction, LAMPORTS_PER_SOL
 } from '@solana/web3.js';
-import base58 from 'bs58';
-import { NextApiRequest, NextApiResponse } from 'next';
 import {
     createMint, getOrCreateAssociatedTokenAccount, Account,
     mintTo, createTransferInstruction, createAccount, getAccount,
-    TOKEN_PROGRAM_ID,
 } from '@solana/spl-token';
+import base58 from 'bs58';
 import testConfig from './config.test.json';
 
 const payerKeypair = Keypair.generate();
@@ -19,7 +17,6 @@ process.env.OCTANE_CONFIG_JSON = JSON.stringify(testConfig);
 
 import transfer from '../pages/api/transfer';
 import blockhash from '../pages/api/blockhash';
-
 
 if (process.env.TEST_LIVE) {
   describe('Transfer endpoint', async () => {
@@ -54,31 +51,12 @@ if (process.env.TEST_LIVE) {
 
       let sourceOwner: Keypair;
       let sourceAccount: PublicKey;
-      let targetOwner: Keypair;
-      let targetAccount: PublicKey;
       let recentBlockhash: string = '';
       beforeEach(async () => {
-          // We shouldn't airdrop any SOL to this keypair
+          // We shouldn't airdrop any SOL to this keypair to prove fee paying works
           sourceOwner = Keypair.generate();
-          sourceAccount = await createAccount(
-              connection,
-              payerKeypair,
-              mint,
-              sourceOwner.publicKey,
-              undefined,
-              undefined,
-              TOKEN_PROGRAM_ID
-          );
-
-          await mintTo(
-              connection,
-              tokenKeypair,
-              mint,
-              sourceAccount,
-              tokenKeypair.publicKey,
-              5000,
-          );
-
+          sourceAccount = await createAccount(connection, payerKeypair, mint,sourceOwner.publicKey);
+          await mintTo(connection, tokenKeypair, mint, sourceAccount, tokenKeypair.publicKey, 5000);
           await testApiHandler({
               handler: blockhash,
               test: async ({ fetch }) => {
@@ -90,10 +68,9 @@ if (process.env.TEST_LIVE) {
           });
       });
 
-      it('signs a transaction with 1 token transfer to Octane payer successfully', async () => {
-          const transaction = new Transaction().add(
-              createTransferInstruction(sourceAccount, tokenAccount.address, sourceOwner.publicKey, 100, [], TOKEN_PROGRAM_ID)
-          );
+      it('signs a transaction with token transfer to Octane payer successfully', async () => {
+          const transaction = new Transaction();
+          transaction.add(createTransferInstruction(sourceAccount, tokenAccount.address, sourceOwner.publicKey, 100));
           transaction.feePayer = payerKeypair.publicKey;
           transaction.recentBlockhash = recentBlockhash;
           transaction.partialSign(sourceOwner);
@@ -103,10 +80,7 @@ if (process.env.TEST_LIVE) {
               test: async ({ fetch }) => {
                   const res = await fetch({
                       method: 'POST',
-                      headers: {
-                          'Accept': 'application/json',
-                          'Content-Type': 'application/json'
-                      },
+                      headers: {'Content-Type': 'application/json'},
                       body: JSON.stringify({
                           transaction: base58.encode(transaction.serialize({requireAllSignatures: false}))
                       })
@@ -117,10 +91,80 @@ if (process.env.TEST_LIVE) {
 
                   expect((await connection.getSignatureStatus(signature)).value!.confirmationStatus).to.be.equals('confirmed');
                   expect((await getAccount(connection, sourceAccount, 'confirmed')).amount).to.equal(BigInt(4900));
+                  expect((await getAccount(connection, tokenAccount.address, 'confirmed')).amount).to.equal(BigInt(100));
               }
           });
       });
-      // test with arbitrary Transfer
+
+      it('signs a transaction with token transfer and an arbitrary transfer to Octane payer successfully', async () => {
+          const targetOwner = Keypair.generate();
+          // We assume target account is already created.
+          const targetAccount = await createAccount(connection, payerKeypair, mint, targetOwner.publicKey);
+
+          const transaction = new Transaction();
+          transaction.add(createTransferInstruction(sourceAccount, tokenAccount.address, sourceOwner.publicKey, 100));
+          transaction.add(createTransferInstruction(sourceAccount, targetAccount, sourceOwner.publicKey, 100));
+          transaction.feePayer = payerKeypair.publicKey;
+          transaction.recentBlockhash = recentBlockhash;
+          transaction.partialSign(sourceOwner);
+
+          await testApiHandler({
+              handler: transfer,
+              test: async ({ fetch }) => {
+                  const res = await fetch({
+                      method: 'POST',
+                      headers: {'Content-Type': 'application/json'},
+                      body: JSON.stringify({
+                          transaction: base58.encode(transaction.serialize({requireAllSignatures: false}))
+                      })
+                  });
+                  expect(res.status).to.be.equals(200);
+                  const signature = (await res.json())['signature'] as string;
+                  expect(signature).to.not.be.empty;
+
+                  expect((await connection.getSignatureStatus(signature)).value!.confirmationStatus).to.be.equals('confirmed');
+                  expect((await getAccount(connection, sourceAccount, 'confirmed')).amount).to.equal(BigInt(4800));
+                  expect((await getAccount(connection, tokenAccount.address, 'confirmed')).amount).to.equal(BigInt(200));
+                  expect((await getAccount(connection, targetAccount, 'confirmed')).amount).to.equal(BigInt(100));
+              }
+          });
+      });
+
+      it('rejects a duplicate transaction', async () => {
+          const transaction = new Transaction();
+          transaction.add(createTransferInstruction(sourceAccount, tokenAccount.address, sourceOwner.publicKey, 100));
+          transaction.feePayer = payerKeypair.publicKey;
+          transaction.recentBlockhash = recentBlockhash;
+          transaction.partialSign(sourceOwner);
+          await testApiHandler({
+              handler: transfer,
+              test: async ({ fetch }) => {
+                  const res = await fetch({
+                      method: 'POST',
+                      headers: {'Content-Type': 'application/json'},
+                      body: JSON.stringify({
+                          transaction: base58.encode(transaction.serialize({requireAllSignatures: false}))
+                      })
+                  });
+                  expect(res.status).to.be.equals(200);
+              }
+          });
+          await testApiHandler({
+              handler: transfer,
+              test: async ({ fetch }) => {
+                  const res = await fetch({
+                      method: 'POST',
+                      headers: {'Content-Type': 'application/json'},
+                      body: JSON.stringify({
+                          transaction: base58.encode(transaction.serialize({requireAllSignatures: false}))
+                      })
+                  });
+                  expect(res.status).to.be.equals(500);
+              }
+          });
+      })
+
       // test of fails
+      // test with arbitrary program call
   });
 }
